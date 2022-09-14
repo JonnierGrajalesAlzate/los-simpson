b611b9e9282f57f1f22547a5637ff92281738c2d
JonnierGrajalesAlzate <105441441+JonnierGrajalesAlzate@users.noreply.github.com>
2022-09-14 16:05:17 -0500

    Add files via upload

---
+++ appSimpson/data/data.js
@@ -0,0 +1,28 @@
+export const data = 
+[
+    {
+        "id": 1,
+        "name": "Bart Simpson",
+        "image": "/images/bart.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Homero Simpson",
+        "image": "/images/homero.png"
+    },
+    {
+        "id": 3,
+        "name": "Lisa Simpson",
+        "image": "/images/lisa.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Maggie Simpson",
+        "image": "/images/maggie.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Marge Bouvier",
+        "image": "/images/marge.jpg"
+    }
+]
